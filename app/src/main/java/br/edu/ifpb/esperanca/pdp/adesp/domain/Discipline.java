88c51d6b389abcab5860519d6c350ab082510c1a
Aniel Melo <103321497+anielmelo@users.noreply.github.com>
2024-03-07 14:05:09 -0300

    Update Discipline.java

--- app/src/main/java/br/edu/ifpb/esperanca/pdp/adesp/domain/Discipline.java
+++ app/src/main/java/br/edu/ifpb/esperanca/pdp/adesp/domain/Discipline.java
@@ -1,6 +1,6 @@
 package br.edu.ifpb.esperanca.pdp.adesp.domain;
 
 public class Discipline {
-    private int id_Discipline;
+    private int id_discipline;
     private String name, course, period;
 }
